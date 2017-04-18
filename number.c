0189df935e3fe24db9b88d879bb48da18fe7ade1
frankipod <yaoxiaochu@gmail.com>
2017-04-17 20:25:56 -0500

    something

--- number.c
+++ number.c
@@ -2,8 +2,12 @@
 
 main()
 {
-int count;
-puts("Please enter a number: ");
-scanf("%d", &count);
-printf("The number is %d \n", count);
+int a;
+int b;
+puts("Please enter an integer a: ");
+scanf("%d", &a);
+puts("please enter another integer b:");
+scanf("%d", &b);
+printf("a + b = %d \n", a+b);
+return a+b;
 }
